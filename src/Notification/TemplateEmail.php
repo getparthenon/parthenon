28652f82897ce51dc3d51826c1161653328cabea
Iain Cambridge <iain@humblyarrogant.io>
2022-10-20 08:03:48 +0000

    #15 - Add TemplateEmail class
    Automatic commit for https://github.com/getparthenon/monorepo/commit/a25ebc3a9fac5c533deb5e3303a092d889c8d42a

---
+++ src/Notification/TemplateEmail.php
@@ -0,0 +1,143 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright Humbly Arrogant Ltd 2020-2022.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
+ *
+ * Change Date: TBD ( 3 years after 2.1.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace Parthenon\Notification;
+
+abstract class TemplateEmail implements EmailInterface
+{
+    private ?string $subject = null;
+
+    private ?string $fromAddress = null;
+
+    private ?string $fromName = null;
+
+    private ?string $toAddress = null;
+
+    private ?string $toName = null;
+
+    private ?string $content = null;
+
+    private array $templateVariables = [];
+
+    /**
+     * @var Attachment[]
+     */
+    private array $attachments = [];
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(?string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getFromAddress(): ?string
+    {
+        return $this->fromAddress;
+    }
+
+    public function setFromAddress(?string $fromAddress): self
+    {
+        $this->fromAddress = $fromAddress;
+
+        return $this;
+    }
+
+    public function getFromName(): ?string
+    {
+        return $this->fromName;
+    }
+
+    public function setFromName(?string $fromName): self
+    {
+        $this->fromName = $fromName;
+
+        return $this;
+    }
+
+    public function getToAddress(): ?string
+    {
+        return $this->toAddress;
+    }
+
+    public function setToAddress(?string $toAddress): self
+    {
+        $this->toAddress = $toAddress;
+
+        return $this;
+    }
+
+    public function getToName(): ?string
+    {
+        return $this->toName;
+    }
+
+    public function setToName(?string $toName): self
+    {
+        $this->toName = $toName;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    abstract public function getTemplateName(): string;
+
+    public function setTemplateVariables(array $templateVariables): self
+    {
+        $this->templateVariables = $templateVariables;
+
+        return $this;
+    }
+
+    public function getTemplateVariables(): array
+    {
+        return $this->templateVariables;
+    }
+
+    public function isTemplate(): bool
+    {
+        return true;
+    }
+
+    public function addAttachment(Attachment $attachment): self
+    {
+        $this->attachments[] = $attachment;
+
+        return $this;
+    }
+
+    /**
+     * @return Attachment[]
+     */
+    public function getAttachments(): array
+    {
+        return $this->attachments;
+    }
+}
---
+++ src/User/Notification/UserTemplateEmail.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright Humbly Arrogant Ltd 2020-2022.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
+ *
+ * Change Date: TBD ( 3 years after 2.1.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace Parthenon\User\Notification;
+
+use Parthenon\Notification\TemplateEmail;
+use Parthenon\User\Entity\UserInterface;
+
+abstract class UserTemplateEmail extends TemplateEmail
+{
+    protected ?UserInterface $user;
+
+    public static function createFromUser(UserInterface $user): self
+    {
+        $self = new static();
+        $self->setToName($user->getName())->setToAddress($user->getEmail());
+        $self->user = $user;
+
+        return $self;
+    }
+
+    public function getTemplateVariables(): array
+    {
+        return array_merge(parent::getTemplateVariables(), ['name' => $this->user->getName()]);
+    }
+}
