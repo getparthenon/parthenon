29d0a4ee46fc598214af7aa1529739bf5616f7ba
Iain Cambridge <iain@humblyarrogant.io>
2022-10-30 13:25:54 +0000

    Change copyright
    Automatic commit for https://github.com/getparthenon/monorepo/commit/ba847fbf2159d6027c766d4b531337e648d1f28d

--- src/User/Repository/ForgotPasswordCodeOdmRepository.php
+++ src/User/Repository/ForgotPasswordCodeOdmRepository.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * Copyright Humbly Arrogant Ltd 2020-2022.
+ * Copyright Iain Cambridge 2020-2022.
  *
  * Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
  *
