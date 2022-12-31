823a87de0632f7801a4f9eb270daef0491f1468e
Iain Cambridge <iain@humblyarrogant.io>
2022-12-31 20:59:30 +0000

    [Billing] Add payment entity skeleton
    Automatic commit for https://github.com/getparthenon/monorepo/commit/7cfe4422f70bb80b37bfcc64e23af86e41b6acbe

---
+++ src/Billing/Entity/Payment.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright Iain Cambridge 2020-2022.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
+ *
+ * Change Date: TBD ( 3 years after 2.2.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace Parthenon\Billing\Entity;
+
+class Payment
+{
+    private $id;
+
+    private string $paymentReference;
+
+    private string $provider;
+
+    private int $amount;
+
+    private string $currency;
+
+    private \DateTimeInterface $createdAt;
+
+    private bool $refunded = false;
+
+    private bool $completed = false;
+
+    private bool $chargedback = false;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getPaymentReference(): string
+    {
+        return $this->paymentReference;
+    }
+
+    public function setPaymentReference(string $paymentReference): void
+    {
+        $this->paymentReference = $paymentReference;
+    }
+
+    public function getProvider(): string
+    {
+        return $this->provider;
+    }
+
+    public function setProvider(string $provider): void
+    {
+        $this->provider = $provider;
+    }
+
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): void
+    {
+        $this->amount = $amount;
+    }
+
+    public function getCurrency(): string
+    {
+        return $this->currency;
+    }
+
+    public function setCurrency(string $currency): void
+    {
+        $this->currency = $currency;
+    }
+
+    public function getCreatedAt(): \DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function isRefunded(): bool
+    {
+        return $this->refunded;
+    }
+
+    public function setRefunded(bool $refunded): void
+    {
+        $this->refunded = $refunded;
+    }
+
+    public function isCompleted(): bool
+    {
+        return $this->completed;
+    }
+
+    public function setCompleted(bool $completed): void
+    {
+        $this->completed = $completed;
+    }
+
+    public function isChargedback(): bool
+    {
+        return $this->chargedback;
+    }
+
+    public function setChargedback(bool $chargedback): void
+    {
+        $this->chargedback = $chargedback;
+    }
+}
---
+++ src/Billing/Repository/PaymentRepositoryInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright Iain Cambridge 2020-2022.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
+ *
+ * Change Date: TBD ( 3 years after 2.2.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace Parthenon\Billing\Repository;
+
+use Parthenon\Common\Repository\RepositoryInterface;
+
+interface PaymentRepositoryInterface extends RepositoryInterface
+{
+}
