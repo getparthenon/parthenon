db436826abd34beb3f117290e122686e689e6de9
Iain Cambridge <iain@humblyarrogant.io>
2023-02-13 20:55:37 +0000

    Fix test
    Automatic commit for https://github.com/getparthenon/monorepo/commit/b0b5e7d010a83a06edc0cd7dd73b437e5ba31086

--- tests/Parthenon/Billing/Obol/BillingDetailsFactory.php
+++ tests/Parthenon/Billing/Obol/BillingDetailsFactory.php
@@ -1,5 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * Copyright Iain Cambridge 2020-2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
+ *
+ * Change Date: TBD ( 3 years after 2.2.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
 namespace Parthenon\Billing\Obol;
 
 use Parthenon\Billing\Entity\CustomerInterface;
@@ -16,7 +28,7 @@ class BillingDetailsFactory extends TestCase
     public const REGION = 'Example';
     public const COUNTRY_CODE = 'US';
     public const POST_CODE = 'GE384';
-    const PAYMENT_REFERENCE = 'payment-reference';
+    public const PAYMENT_REFERENCE = 'payment-reference';
 
     public function testConvertCustomer()
     {
--- tests/Parthenon/Billing/Obol/PaymentFactoryTest.php
+++ tests/Parthenon/Billing/Obol/PaymentFactoryTest.php
@@ -1,5 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * Copyright Iain Cambridge 2020-2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
+ *
+ * Change Date: TBD ( 3 years after 2.2.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
 namespace Parthenon\Billing\Obol;
 
 use Brick\Money\Money;
@@ -28,7 +40,7 @@ class PaymentFactoryTest extends TestCase
 
         $subject = new PaymentFactory();
 
-        $actual =  $subject->fromSubscriptionConfirm($customer, $subscriptionCreation);
+        $actual = $subject->fromSubscriptionConfirm($customer, $subscriptionCreation);
         $this->assertTrue($amount->isEqualTo($actual->getMoneyAmount()));
         $this->assertEquals('payment-reference', $actual->getPaymentReference());
         $this->assertSame($customer, $actual->getCustomer());
