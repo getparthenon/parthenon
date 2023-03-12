8ee5116334cdd488a9f73eac82590ed361a3fe71
Iain <iain@humblyarrogant.io>
2023-03-12 15:01:38 +0000

    [Athena] Fix bug in ResultSet getLastKey when no results
    Automatic commit for https://github.com/getparthenon/monorepo/commit/df098231d04b7327c5eda6a82fa14167e5cad33d

--- src/Athena/ResultSet.php
+++ src/Athena/ResultSet.php
@@ -64,6 +64,9 @@ final class ResultSet
     {
         $results = $this->getResults();
         $lastItem = end($results);
+        if (false === $lastItem) {
+            return null;
+        }
 
         return $this->getFieldData($lastItem, $this->sortKey);
     }
