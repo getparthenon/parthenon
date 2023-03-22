41b46f896989c6a94f0b5dafbb3098757803a27e
Iain <iain@humblyarrogant.io>
2023-03-22 09:07:19 +0000

    [Billing] Start SubscriptionPlan and SubscriptionLimit
    Automatic commit for https://github.com/getparthenon/monorepo/commit/b0e71f3f39595f72da86f5d65ce371fca82685ec

---
+++ src/Billing/Entity/SubscriptionPlan.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright Iain Cambridge 2020-2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
+ *
+ * Change Date: TBD ( 3 years after 2.2.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace Parthenon\Billing\Entity;
+
+class SubscriptionPlan
+{
+    private $id;
+
+    private bool $public;
+
+    private string $name;
+
+    private string $externalReference;
+}
