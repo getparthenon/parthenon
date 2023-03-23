fe5069c99684f2fc6b6600ac3478f504cf7ba766
Iain <iain@humblyarrogant.io>
2023-03-23 10:32:11 +0000

    [Billing] Add subscription plan features
    Automatic commit for https://github.com/getparthenon/monorepo/commit/d18c444021c3b4384bcfe481c409a7c561176289

---
+++ src/Billing/Athena/SubscriptionFeatureSection.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright Iain Cambridge 2020-2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
+ *
+ * Change Date: TBD ( 3 years after 2.2.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace Parthenon\Billing\Athena;
+
+use Parthenon\Athena\AbstractSection;
+use Parthenon\Athena\EntityForm;
+use Parthenon\Athena\ListView;
+use Parthenon\Athena\ReadView;
+use Parthenon\Athena\Repository\CrudRepositoryInterface;
+use Parthenon\Billing\Entity\SubscriptionFeature;
+use Parthenon\Billing\Repository\SubscriptionFeatureRepositoryInterface;
+
+class SubscriptionFeatureSection extends AbstractSection
+{
+    public function __construct(private SubscriptionFeatureRepositoryInterface $featureRepository)
+    {
+    }
+
+    public function getUrlTag(): string
+    {
+        return 'billing-subscription-features';
+    }
+
+    public function getRepository(): CrudRepositoryInterface
+    {
+        return $this->featureRepository;
+    }
+
+    public function getEntity()
+    {
+        return new SubscriptionFeature();
+    }
+
+    public function getMenuSection(): string
+    {
+        return 'Billing';
+    }
+
+    public function getMenuName(): string
+    {
+        return 'Subscription Features';
+    }
+
+    public function buildReadView(ReadView $readView): ReadView
+    {
+        $readView->section('Main')
+                ->field('name')
+                ->field('code')
+                ->field('description')
+            ->end();
+
+        return $readView;
+    }
+
+    public function buildEntityForm(EntityForm $entityForm): EntityForm
+    {
+        $entityForm->section('Main')
+                ->field('code')
+                ->field('name')
+                ->field('description')
+            ->end();
+
+        return $entityForm;
+    }
+
+    public function buildListView(ListView $listView): ListView
+    {
+        $listView->addField('name', 'text')
+            ->addField('code', 'text')
+            ->addField('description', 'text');
+
+        return $listView;
+    }
+}
---
+++ src/Billing/Repository/Orm/SubscriptionFeatureServiceRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright Iain Cambridge 2020-2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
+ *
+ * Change Date: TBD ( 3 years after 2.2.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace Parthenon\Billing\Repository\Orm;
+
+use Doctrine\Persistence\ManagerRegistry;
+use Parthenon\Billing\Entity\SubscriptionFeature;
+use Parthenon\Common\Repository\CustomServiceRepository;
+
+class SubscriptionFeatureServiceRepository extends CustomServiceRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, SubscriptionFeature::class);
+    }
+}
