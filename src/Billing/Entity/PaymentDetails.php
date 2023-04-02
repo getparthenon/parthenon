fd42c3e2f1c9ecd0aaea60cf21af1a2265b04d2c
Iain <iain@humblyarrogant.io>
2023-04-02 13:58:11 +0000

    [Billing] FIx billing entity
    Automatic commit for https://github.com/getparthenon/monorepo/commit/4911f13e2183bb8f8ebf1c08450f7ebef77e7308

--- src/Billing/Entity/PaymentDetails.php
+++ src/Billing/Entity/PaymentDetails.php
@@ -112,7 +112,7 @@ class PaymentDetails
         return $this->name;
     }
 
-    public function setName(string $name): void
+    public function setName(?string $name): void
     {
         $this->name = $name;
     }
