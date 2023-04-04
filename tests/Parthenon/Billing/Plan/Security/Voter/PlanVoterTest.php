bbf24bd34f0f27cd873c40e5deb4a7c2c5bd5fdc
Iain <iain@humblyarrogant.io>
2023-04-04 10:13:31 +0000

    [Billing] Add test for plan voter
    Automatic commit for https://github.com/getparthenon/monorepo/commit/b9dae674cbfa6bb99e0abd644a2235cc1c459553

---
+++ tests/Parthenon/Billing/Plan/Security/Voter/PlanVoterTest.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright Iain Cambridge 2020-2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
+ *
+ * Change Date: TBD ( 3 years after 2.2.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace Parthenon\Billing\Plan\Security\Voter;
+
+use Parthenon\Billing\CustomerProviderInterface;
+use Parthenon\Billing\Entity\CustomerInterface;
+use Parthenon\Billing\Plan\CounterInterface;
+use Parthenon\Billing\Plan\CounterManager;
+use Parthenon\Billing\Plan\CustomerPlanInfoInterface;
+use Parthenon\Billing\Plan\LimitableInterface;
+use Parthenon\Billing\Plan\LimitedUserInterface;
+use Parthenon\User\Entity\User;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class PlanVoterTest extends TestCase
+{
+    public function testDoesNotHaveLimit()
+    {
+        $token = $this->createMock(TokenInterface::class);
+        $counterManager = $this->createMock(CounterManager::class);
+        $counter = $this->createMock(CounterInterface::class);
+        $customerProvider = $this->createMock(CustomerProviderInterface::class);
+        $customer = $this->createMock(CustomerInterface::class);
+        $customerPlanInfo = $this->createMock(CustomerPlanInfoInterface::class);
+        $limitable = $this->createMock(LimitableInterface::class);
+
+        $limitable->method('getLimitableName')->willReturn('limit');
+        $counterManager->method('getCounter')->with($limitable)->willReturn($counter);
+        $customerProvider->method('getCurrentCustomer')->willReturn($customer);
+        $customerPlanInfo->method('getLimitCount')->with($customer, 'limit')->willReturn(0);
+        $counter->method('getCount')->willReturn(1);
+
+        $member = new class() extends User implements LimitedUserInterface {
+            public function getPlanName(): string
+            {
+                return 'plan';
+            }
+        };
+
+        $token->method('getUser')->willReturn($member);
+
+        $subject = new PlanVoter(
+            $counterManager,
+            $customerProvider,
+            $customerPlanInfo,
+        );
+
+        $this->assertEquals(PlanVoter::ACCESS_DENIED, $subject->vote($token, $limitable, ['create']));
+    }
+
+    public function testDoesHaveLimit()
+    {
+        $token = $this->createMock(TokenInterface::class);
+        $counterManager = $this->createMock(CounterManager::class);
+        $counter = $this->createMock(CounterInterface::class);
+        $customerProvider = $this->createMock(CustomerProviderInterface::class);
+        $customer = $this->createMock(CustomerInterface::class);
+        $customerPlanInfo = $this->createMock(CustomerPlanInfoInterface::class);
+        $limitable = $this->createMock(LimitableInterface::class);
+
+        $limitable->method('getLimitableName')->willReturn('limit');
+        $counterManager->method('getCounter')->with($limitable)->willReturn($counter);
+        $customerProvider->method('getCurrentCustomer')->willReturn($customer);
+        $customerPlanInfo->method('getLimitCount')->with($customer, 'limit')->willReturn(10);
+        $counter->method('getCount')->willReturn(1);
+
+        $member = new class() extends User implements LimitedUserInterface {
+            public function getPlanName(): string
+            {
+                return 'plan';
+            }
+        };
+
+        $token->method('getUser')->willReturn($member);
+
+        $subject = new PlanVoter(
+            $counterManager,
+            $customerProvider,
+            $customerPlanInfo,
+        );
+
+        $this->assertEquals(PlanVoter::ACCESS_GRANTED, $subject->vote($token, $limitable, ['create']));
+    }
+}
