fb10b08e94304688eeefe1ac71e17a2c15546089
Iain <iain@humblyarrogant.io>
2023-05-31 15:53:00 +0000

    [Billing] Add subscription cancelled
    Automatic commit for https://github.com/getparthenon/monorepo/commit/4f1fdf3c4b8c02a94162cfbe76a7e865c6b79ce7

--- src/Billing/Subscription/SubscriptionManager.php
+++ src/Billing/Subscription/SubscriptionManager.php
@@ -24,6 +24,7 @@ use Parthenon\Billing\Entity\Subscription;
 use Parthenon\Billing\Entity\SubscriptionPlan;
 use Parthenon\Billing\Enum\SubscriptionStatus;
 use Parthenon\Billing\Event\PaymentCreated;
+use Parthenon\Billing\Event\SubscriptionCancelled;
 use Parthenon\Billing\Event\SubscriptionCreated;
 use Parthenon\Billing\Exception\SubscriptionCreationException;
 use Parthenon\Billing\Factory\EntityFactoryInterface;
@@ -156,6 +157,9 @@ final class SubscriptionManager implements SubscriptionManagerInterface
         $subscription->setStatus(SubscriptionStatus::PENDING_CANCEL);
         $subscription->endAtEndOfPeriod();
 
+        $this->subscriptionRepository->save($subscription);
+        $this->dispatcher->dispatch(new SubscriptionCancelled($subscription), SubscriptionCancelled::NAME);
+
         return $subscription;
     }
 
@@ -173,6 +177,9 @@ final class SubscriptionManager implements SubscriptionManagerInterface
         $subscription->setActive(false);
         $subscription->endNow();
 
+        $this->subscriptionRepository->save($subscription);
+        $this->dispatcher->dispatch(new SubscriptionCancelled($subscription), SubscriptionCancelled::NAME);
+
         return $subscription;
     }
 
@@ -189,6 +196,9 @@ final class SubscriptionManager implements SubscriptionManagerInterface
         $subscription->setStatus(SubscriptionStatus::PENDING_CANCEL);
         $subscription->setEndedAt($dateTime);
         $subscription->setValidUntil($dateTime);
+
+        $this->subscriptionRepository->save($subscription);
+        $this->dispatcher->dispatch(new SubscriptionCancelled($subscription), SubscriptionCancelled::NAME);
 
         return $subscription;
     }
