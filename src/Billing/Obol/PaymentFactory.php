d321c8e77fedc583c44617aa3980682f9b89df11
Iain <iain@humblyarrogant.io>
2024-01-04 22:11:47 +0000

    Update copyright header
    Automatic commit for https://github.com/getparthenon/monorepo/commit/fb5911e9252f67284dd68897e936dd7434b41847

--- src/Billing/Obol/PaymentFactory.php
+++ src/Billing/Obol/PaymentFactory.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * Copyright Humbly Arrogant Software Limited 2020-2023.
+ * Copyright Humbly Arrogant Software Limited 2020-2024
  *
  * Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
  *
