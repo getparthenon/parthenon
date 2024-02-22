9f9f0bbfacc5dd6a61dd865dcf2111a641a22377
Iain Cambridge <iain@humblyarrogant.io>
2024-02-22 20:16:33 +0100

    Update Configuration.php

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -32,7 +32,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('parthenon');
 
