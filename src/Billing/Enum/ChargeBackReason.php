58e267c738129db269256134e4ba9b28afa6004d
Iain <iain@humblyarrogant.io>
2025-01-06 14:36:54 +0000

    Bump copyright header
    Automatic commit for https://github.com/getparthenon/monorepo/commit/24c8def059c14b917f3713c63b6a62f631d774be

--- src/Billing/Enum/ChargeBackReason.php
+++ src/Billing/Enum/ChargeBackReason.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * Copyright Iain Cambridge 2020-2025.
+ * Copyright (C) 2020-2025 Iain Cambridge
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
